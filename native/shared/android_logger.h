57c88693acb83f54f3578d995ab686ac2d9a32ac
Eduard Maximovich <eduard.maximovich@pure.app>
2026-01-23 18:35:57 +0100

    update Android logger implementation and CI configuration for Ubuntu 22.04

--- native/shared/android_logger.h
+++ native/shared/android_logger.h
@@ -4,13 +4,13 @@
 
 #ifndef LEVELDB_JNI_ANDROID_LOGGER_H
 #define LEVELDB_JNI_ANDROID_LOGGER_H
+#include <android/log.h>
 #include "leveldb/env.h"
-
 // Redirects leveldb's logging to the Android logger.
 class AndroidLogger final : public leveldb::Logger {
-public:
-    void Logv(const char* format, va_list ap) override {
-        //        __android_log_vprint(ANDROID_LOG_INFO, "com.edwardstock.leveldb:N", format, ap);
+ public:
+  void Logv(const char *format, va_list ap) override {
+    __android_log_vprint(ANDROID_LOG_INFO, "com.edwardstock.leveldb:N", format, ap);
     }
 };
 
